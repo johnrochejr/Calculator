1a810ba89db9a75ef0ad53bd87e447bb9019f81b
John Roche <roche.johnjr@gmail.com>
2017-07-20 15:31:47 -0400

    Redoing HTML

--- main.js
+++ main.js
@@ -1,38 +1,74 @@
- 1. Create basic structure HTML
-    Static HTML: the calculator itself, 5 rows of buttons, 4 columns
-    I will have to flex out a collection of divs that sit under one wrapper
-    allowing me to flex them accordingly.
+
+ //
+ //  3. From there, I am going to select nodes with JS to add functionality to each
+ //  button. Using functions and algorithsm, I need to creat code for:
+ //    A. 'Clear' - Clears whatever content has been input into the calculator, causing
+ //      the 'results screen' to revert back to the value 0.
+ //    B. 'Results screen' - Whatever numbers or actions are taken, the resulting
+ //      information will show up here. I need to figure out how to link the screen
+ //      to all the other buttons, and ensure the info is logged correctly.
+ //    C. Number buttons 1 - 9: Much like blackjack calculator, the values of these
+ //      buttons correspond directly to the number.
+ //    D. Math buttons (division, mult., sub, add) - Action buttons that will perform
+ //      a task on whatver numbers are put into the calculator
+ //    E. Equals - button that totals whatver math action that has been put in the
+ //      system
+ //    F. Decimal - adds a decimal to numbers.
+ //
+ //
+ //
+ //  4. Once I have statically created each button with HTML, I will use JS to add
+ //    event listeners for click and keyboard input (using number pad, /, X, -, +, =, .)
+ //
+ //
+ //
+ //  5. I have to devise algorithms that actually accomplish whatever math action
+ //    has been taken
+ //
+ //
+ //
+ //   I am having a tough time mapping out which nodes I need to select and how
+ //   dynamic content needs to be added to my calculator
+ //
+
+ //   I just spoke with Gavin:
+ //   For now - focus on getting the '1 button' to listen to a click, and then
+ //   log its value to the display screen
+
+ //   for all buttons, one event listener  with a for loop
+ //   iterate through the buttons.length
+
+ //   for event listener: click, then function
+
+ let button = document.querySelector(".num").value;
+
+ console.log(button);
 
 
-  2. Add some CSS styling so I have a basic framework to work off of
-
-
-  3. From there, I am going to select nodes with JS to add functionality to each
-  button. Using functions and algorithsm, I need to creat code for:
-    A. 'Clear' - Clears whatever content has been input into the calculator, causing
-      the 'results screen' to revert back to the value 0.
-    B. 'Results screen' - Whatever numbers or actions are taken, the resulting
-      information will show up here. I need to figure out how to link the screen
-      to all the other buttons, and ensure the info is logged correctly.
-    C. Number buttons 1 - 9: Much like blackjack calculator, the values of these
-      buttons correspond directly to the number.
-    D. Math buttons (division, mult., sub, add) - Action buttons that will perform
-      a task on whatver numbers are put into the calculator
-    E. Equals - button that totals whatver math action that has been put in the
-      system
-    F. Decimal - adds a decimal to numbers.
 
 
 
-  4. Once I have statically created each button with HTML, I will use JS to add
-    event listeners for click and keyboard input (using number pad, /, X, -, +, =, .)
 
 
 
-  5. I have to devise algorithms that actually accomplish whatever math action
-    has been taken
 
 
 
-    I am having a tough time mapping out which nodes I need to select and how
-    dynamic content needs to be added to my calculator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //
