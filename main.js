fb04d4a8acf9f66b173a6cb6732ecf0fefc97671
John Roche <roche.johnjr@gmail.com>
2017-08-05 15:17:08 -0400

    redoing values

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let input = [];
 // Select/grab where we want our info sent to (dynamically)
 let display = document.getElementById("display-text");
 
-function calc(value) {
+function calc(obj) {
 	// create a function 'calc' to push value to input
 	// ... inputed by our 1 button. "1" is a string!
 	// we use parseInt to change string to integer
@@ -14,28 +14,32 @@ function calc(value) {
 	// .. loose equality compares two values for equality, after
 	//... converting both values to a commmon type
 	// convert data type
-	if (value == "1") {
-		value = parseInt(value);
-		input.push(value);
-		display.innerHTML = input; // sets value
+	if (obj.className === "num") {
+		// value = parseInt(obj);
+		input.push(parseInt(obj.value));
+		// display.innerHTML = input; // sets value
+		console.log(obj.value);
 		// InnerHTML property sets or gets HTML syntax describing
 		// the element's descendants;
 		// this logs 1 to display
-	} else if (value === "C") {
+	} else if (obj.className === "cancel") {
 		// this is clear!
 		input = [];
 		display.innerHTML = input;
 		// resets array to empty []
-	} else if (value === "/" || value === "-") {
+	} else if (obj === "/" || obj === "-") {
 		//still working here
-	} else if (value === "=") {
+	} else if (obj === "=") {
 		input = [];
 		// = resets to empty array
 		display.innerHTML = "Working..."
+		console.log(display.innerHTML);
 	}
-	console.log(input);
-	input.push(value);
+	display.innerHTML = input;
+	return input;
+	// console.log(input);
+	// input.push(value);
 	// pushes value to input
-	display.innerHTML = input
+	// display.innerHTML = input
 	// changes innerHTML of display to input
 };
