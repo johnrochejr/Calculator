4d7cdaa6113d616fde31e486261df521d7a965e8
John Roche <roche.johnjr@gmail.com>
2017-08-05 16:12:58 -0400

    put each element in its own array

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // Variable to hold input
 // Need to create an empty array to push inputs to!
-let input = [];
-
+// let input = [];
+let input = "";
+let inputsReadyToCalc = [];
 // Variable to hold inputs ready to calculate
 // Select/grab where we want our info sent to (dynamically)
 let display = document.getElementById("display-text");
@@ -15,25 +16,43 @@ function calc(obj) {
 	//... converting both values to a commmon type
 	// convert data type
 	if (obj.className === "num") {
+		// addition assignment adds the value of the
+		// ... right operand to a variable and assigns
+		// ... the result to the variable
+		input += obj.value;
+		console.log(input);
 		// value = parseInt(obj);
-		input.push(parseInt(obj.value));
+		// input.push(parseInt(obj.value));
 		// display.innerHTML = input; // sets value
-		console.log(obj.value);
+		// console.log(obj.value);
 		// InnerHTML property sets or gets HTML syntax describing
 		// the element's descendants;
 		// this logs 1 to display
 	} else if (obj.className === "cancel") {
 		// this is clear!
-		input = [];
+		input = "";
+		inputsReadyToCalc = [];
 		display.innerHTML = input;
 		// resets array to empty []
-	} else if (obj === "/" || obj === "-") {
-		//still working here
-	} else if (obj === "=") {
-		input = [];
+	} else if (obj.className === "operator"
+	&& input.length != 0) {
+		// if an operator button is clicked
+		// and something has been input
+		inputsReadyToCalc.push(input, obj.value)
+		// push the input and value of parameter to empty
+		// array
+		console.log(inputsReadyToCalc);
+	} else if (obj.className === "evaluate"
+	&& input.length != 0) {
+		let total = 0;
+		for (let i = 0; i < inputsReadyToCalc.length; i++) {
+			console.log(total);
+			return total;
+		}
+		// input = [];
 		// = resets to empty array
-		display.innerHTML = "Working..."
-		console.log(display.innerHTML);
+		// display.innerHTML = "Working..."
+		// console.log(display.innerHTML);
 	}
 	display.innerHTML = input;
 	return input;
