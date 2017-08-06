9f05aae810b303a3acbec2e8cd8cd78a2e608c56
John Roche <roche.johnjr@gmail.com>
2017-08-05 20:19:06 -0400

    switch case

--- main.js
+++ main.js
@@ -20,10 +20,10 @@ function calc(obj) {
 		// ... right operand to a variable and assigns
 		// ... the result to the variable
 		input += obj.value;
+		display.innerHTML = input; // sets value
 		console.log(input);
 		// value = parseInt(obj);
 		// input.push(parseInt(obj.value));
-		// display.innerHTML = input; // sets value
 		// console.log(obj.value);
 		// InnerHTML property sets or gets HTML syntax describing
 		// the element's descendants;
@@ -36,29 +36,110 @@ function calc(obj) {
 		// resets array to empty []
 	} else if (obj.className === "operator"
 	&& input.length != 0) {
+		// Display operator but doesn't save
+		display.innerHTML = obj.value;
+		// console.log(obj.value);
+		// Save last input and the operator that was
+		// ... put in
+		inputsReadyToCalc.push(parseInt(input), obj.value);
+		// reset input
+		input = "";
 		// if an operator button is clicked
 		// and something has been input
-		inputsReadyToCalc.push(input, obj.value)
+		// inputsReadyToCalc.push(input, obj.value)
 		// push the input and value of parameter to empty
 		// array
 		console.log(inputsReadyToCalc);
 	} else if (obj.className === "evaluate"
 	&& input.length != 0) {
-		let total = 0;
-		for (let i = 0; i < inputsReadyToCalc.length; i++) {
-			console.log(total);
-			return total;
+		inputsReadyToCalc.push(parseInt(input), obj.value);
+		total = evaluate(inputsReadyToCalc);
+		// The eval() function evaluates JavaScript code
+		// ... represented as a string.
+		display.innerHTML = total;
+		input = "";
+		inputsReadyToCalc = [];
+		input += total;
 		}
+
+
+	return input;
 		// input = [];
 		// = resets to empty array
 		// display.innerHTML = "Working..."
 		// console.log(display.innerHTML);
-	}
-	display.innerHTML = input;
-	return input;
+};
+	// display.innerHTML = input;
+
 	// console.log(input);
 	// input.push(value);
 	// pushes value to input
 	// display.innerHTML = input
 	// changes innerHTML of display to input
+
+
+function evaluate(inputsReadyToCalc) {
+	for (let i = 0; i < inputsReadyToCalc.length; i += 3) {
+		let term1 = inputsReadyToCalc[i];
+		let op = inputsReadyToCalc[i + 1];
+		let term2 = inputsReadyToCalc[i + 2];
+		let total = 0;
+//
+// 		// calculate value:
+// 		// The switch statement evaluates an expression,
+// 		// matching the expression's value to a case clause,
+// 		// and executes statements associated with that case.
+		switch (op) {
+			case "+":
+				total = term1 + term2;
+				break;
+			case "-":
+				total = term1 - term2;
+				break;
+			case "/":
+				total = term1 / term2;
+				break;
+			case "*":
+				total = term1 * term2;
+				break;
+			default:
+				null;
+		}
+		console.log(total);
+		return total;
+	}
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
